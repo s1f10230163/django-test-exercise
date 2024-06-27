2e2f4b104858ed2c25ead46cdc932a0da08da439
1F10230163 <s1f102301639@iniad.org>
2024-06-27 17:56:24 +0900

    テストを追加（修正）

--- todo/tests.py
+++ todo/tests.py
@@ -58,6 +58,15 @@ class TodoViewTestCase (TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
+
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        response = client.post('/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 1)
     
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware (datetime (2024, 7, 1)))
